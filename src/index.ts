9278a17b2e246acaa0be63fbe6c80191dd260e15
Jonas-Merlin Schumacher <jonas.merlin.schumacher@mailbox.org>
2024-03-03 11:52:22 +0100

    improvement: re-export Props as SEOProps

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export { default as SEO } from "./SEO.astro";
 
 // @ts-ignore
 export * from "./SEO.astro";
+
+// @ts-ignore
+import { Props } from "./SEO.astro";
+
+// @ts-ignore
+export type SEOProps = Props;
