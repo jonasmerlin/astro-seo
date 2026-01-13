0c706e831c6eb7f28a12a25ef6914880d855e77a
Jonas-Merlin Schumacher <jonas.merlin.schumacher@mailbox.org>
2026-01-13 15:58:10 +0100

    chore: Fix warnings

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
+import cypressPlugins from './cypress/plugins/index.js'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return cypressPlugins(on, config)
     },
   },
 })
